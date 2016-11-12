d6f36bcbfd3c03d47fd74d23f06376068f3b287f
nabedge <nabedge@gmail.com>
2016-11-13 01:30:54 +0900

    spring boot 1.4

---
+++ src/main/resources/archetype-resources/src/main/java/config/AppConfig.java
@@ -0,0 +1,31 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.config;
+
+import org.mixer2.Mixer2Engine;
+import org.mixer2.spring.webmvc.Mixer2XhtmlViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+    @Bean
+    public Mixer2Engine mixer2Engine() {
+        return new Mixer2Engine();
+    }
+
+    @Bean
+    public Mixer2XhtmlViewResolver mixer2XhtmlViewResolver() {
+        Mixer2XhtmlViewResolver resolver = new Mixer2XhtmlViewResolver();
+        resolver.setPrefix("classpath:/m2template/");
+        resolver.setSuffix(".html");
+        resolver.setBasePackage("${package}.view");
+        resolver.setMixer2Engine(mixer2Engine());
+        resolver.setOrder(100);
+        return resolver;
+    }
+}
--- src/main/resources/archetype-resources/src/test/java/view/IndexViewTest.java
+++ src/main/resources/archetype-resources/src/test/java/view/IndexViewTest.java
@@ -3,65 +3,55 @@
 #set( $symbol_escape = '\' )
 package ${package}.view;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-
+import ${package}.config.AppConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mixer2.Mixer2Engine;
 import org.mixer2.jaxb.xhtml.Div;
 import org.mixer2.jaxb.xhtml.Html;
-import org.mixer2.spring.webmvc.AbstractMixer2XhtmlView;
-import org.mixer2.spring.webmvc.Mixer2XhtmlViewResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.servlet.View;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:applicationContext.xml",
-        "classpath:mvc-dispatcher-servlet.xml" })
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = {AppConfig.class})
 public class IndexViewTest {
 
     @Autowired
-    private Mixer2XhtmlViewResolver resolver;
-    
-    @Autowired
     private Mixer2Engine mixer2Engine;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private String template = "classpath:/m2template/index.html";
+
     @Test
-    public void testIndexView() throws Exception {
-        
-        View view = resolver.resolveViewName("index", null);
-        assertTrue(view instanceof AbstractMixer2XhtmlView);
-        AbstractMixer2XhtmlView m2View = (AbstractMixer2XhtmlView) view;
+    public void test() throws Exception {
+        IndexView indexView = new IndexView();
+        wac.getAutowireCapableBeanFactory().autowireBean(indexView);
 
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        MockHttpServletResponse response = new MockHttpServletResponse();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(
-                request));
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        MockHttpServletResponse res = new MockHttpServletResponse();
 
-        Map<String, Object> model = new HashMap<String, Object>();
-        model.put("helloMessage", "Hello World !");
+        Html tmpl = mixer2Engine.loadHtmlTemplate(resourceLoader.getResource(template).getInputStream());
+        ExtendedModelMap model = new ExtendedModelMap();
+        model.addAttribute("helloMessage", "foo");
 
-        m2View.render(model, request, response);
-        Html html = mixer2Engine.loadHtmlTemplate(response.getContentAsString());
+        Html html = indexView.renderHtml(tmpl, model, req, res);
 
-        // assert
-        Div div = html.getById("message", Div.class);
-        // item name
-        assertThat(div.getContent().get(0)
-                .toString(), is("Hello World !"));
-
+        assertThat(html.getById("message", Div.class).getContent().get(0).toString()).isEqualTo("foo");
     }
-
 }
